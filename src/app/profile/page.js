ffa6a2daf1784dd9f9b3284e7f3304fd445c24f0
Souvik-1705 <souvik.techdev1705@gmail.com>
2026-01-14 22:47:53 +0530

    made changes to profile page

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,27 +1,36 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "@/styles/profile.css";
 import { getUserFromToken } from "@/utils/auth";
 
 export default function ProfilePage() {
   const router = useRouter();
-  const user=getUserFromToken();
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     const token = localStorage.getItem("userToken");
+
     if (!token) {
       router.push("/login");
+      return;
     }
+
+    const userData = getUserFromToken();
+    setUser(userData);
   }, [router]);
+
+  if (!user) {
+    return <p>Loading profile...</p>;
+  }
 
   return (
     <div className="profile-container">
       <div className="profile-avatar">👤</div>
 
       <h1>My Profile</h1>
-      <p><strong>Email:</strong>{user?.email}</p>
+      <p><strong>Email:</strong> {user.email}</p>
       <p><strong>Member Since:</strong> 2024</p>
     </div>
   );
